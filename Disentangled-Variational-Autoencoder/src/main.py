3a3fead1071392367e6fae122117f667435e135b
molnarszilard <molnarszilard@gmail.com>
2021-12-07 12:17:06 +0200

    add Disentangled-Variational-Autoencoder project as 2d VAE tutorial

---
+++ Disentangled-Variational-Autoencoder/src/main.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from pytorch_lightning import Trainer
+
+import logging
+import sys
+sys.path.append("./models")
+sys.path.append("./utils")
+
+from interface import *
+from utils import *
+from vae import VAE
+from beta_vae import betaVAE
+
+
+def main(args):
+    """ main() driver function """
+
+    # Parameters parsing
+    if filepath_is_not_valid(args.config):
+        logging.error("The path {} is not a file. Aborting..".format(args.config))
+        exit()
+
+    configuration, architecture, hyperparameters = parse_config_file(args.config, args.variation)
+    dataset_info = prepare_dataset(configuration)
+    if (dataset_info is None):
+        exit()
+
+    # Initialization
+    model = None
+    if (args.variation == "VAE"):
+        model = VAE(architecture, hyperparameters, dataset_info)
+    elif (args.variation == "B-VAE"):
+        model = betaVAE(architecture, hyperparameters, dataset_info)
+
+    # here you can change the gpus parameter into the amount of gpus you want the model to use
+    trainer = Trainer(max_epochs = hyperparameters["epochs"], gpus=None, fast_dev_run=False)
+
+    # Training and testing
+    trainer.fit(model)
+    result = trainer.test(model)
+    # Model needs to be transferred to the cpu as sample and reconstruct are custom methods
+    model = model.cpu()
+    model.sample(5)
+    model.reconstruct(5)
+
+if __name__ == "__main__":
+    """ call main() function here """
+    print()
+    # configure the level of the logging and the format of the messages
+    logging.basicConfig(level=logging.ERROR, format="%(levelname)s: %(message)s\n")
+    # parse the command line input
+    args = parse_cmd_args()
+    # call the main() driver function
+    main(args)
+    print("\n")
---
+++ Disentangled-Variational-Autoencoder/src/models/beta_vae.py
@@ -0,0 +1,81 @@
+"""
+Implements the betaVAE from the paper: https://arxiv.org/pdf/1606.05579.pdf
+
+Notation used:
+
+    N: Batch Size
+    C: Number of Channels
+    H: Height (of picture)
+    W: Width (of picture)
+    z_dim: The dimension of the latent vector
+
+"""
+import sys
+sys.path.append("../utils")
+
+import torch
+import torch.nn as nn
+
+from vae import VAE
+from model_utils import *
+from utils import *
+
+
+class betaVAE(VAE):
+    """
+    Class that implements a Disentangled Variational Autoencoder (Beta VAE).
+    This class inherits from the VAE class.
+    """
+
+    def __init__(self, architecture, hyperparameters, dataset_info):
+        """
+        :param dict architecture:     A dictionary containing the hyperparameters that define the
+                                        architecture of the model.
+        :param dict hyperparameters:   A dictionary containing the hyperparameters of the model.
+        :param dict dataset_info:      A dictionary containing information about the input dataset.
+
+        The constructor of the Disentangled Variational Autoencoder.
+        """
+
+        # invoke the constructor of the VAE class, as the architecture is the same
+        super(betaVAE, self).__init__(architecture, hyperparameters, dataset_info)
+
+        # store the value of beta in the class as it exists only in this VAE variation
+        self.beta = hyperparameters["beta"]
+
+    @staticmethod
+    def criterion(X, X_hat, mean, std):
+        """
+        :param Tensor X:      The original input data that was passed to the B-VAE.
+                                (N, input_shape[1], H, W)
+        :param Tensor X_hat:  The reconstructed data, the output of the B-VAE.
+                                (N, input_shape[1], H, W)
+        :param Tensor mean:   The output of the mean layer, computed with the output of the
+                                encoder. (N, z_dim)
+        :param Tensor std:    The output of the standard deviation layer, computed with the output
+                                of the encoder. (N, z_dim)
+
+        :return:  A dictionary containing the values of the losses computed.
+        :rtype:   dict
+
+        This method computes the loss of the B-VAE using the formula:
+
+            L(x, x_hat) = - E_{z ~ q_{phi}(z | x)}[log(p_{theta}(x|z))]
+                          + beta * D_{KL}[q_{phi}(z | x) || p_{theta}(x)]
+
+        Intuitively, the expectation term is the Data Fidelity term, and the second term is a
+        regularizer that makes sure the distribution of the encoder and the decoder stay close.
+        """
+        # get the 2 losses
+        data_fidelity_loss = VAE._data_fidelity_loss(X, X_hat)
+        kl_divergence_loss = VAE._kl_divergence_loss(mean, std)
+
+        # add them to compute the loss for each training example in the mini batch
+        loss = -data_fidelity_loss + self.beta * kl_divergence_loss
+
+        # place them all inside a dictionary and return it
+        losses = {"data_fidelity": torch.mean(data_fidelity_loss),
+                  "kl-divergence": torch.mean(kl_divergence_loss),
+                  "beta_kl-divergence": self.beta * torch.mean(kl_divergence_loss),
+                  "loss": torch.mean(loss)}
+        return losses
