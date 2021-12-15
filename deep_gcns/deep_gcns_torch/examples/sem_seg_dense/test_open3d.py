0d0f9cc445d03badcb62e1b7c93b22b96515774e
Funderburger <pop_marian_l@yahoo.com>
2021-12-15 10:19:06 +0200

    Possibly adding for the first time 3D demo...

---
+++ deep_gcns/deep_gcns_torch/examples/sem_seg_dense/test_open3d.py
@@ -0,0 +1,111 @@
+import __init__
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch_geometric.datasets as GeoData
+from torch_geometric.data import DenseDataLoader
+import torch_geometric.transforms as T
+from config import OptInit
+from architecture import DenseDeepGCN
+from utils.ckpt_util import load_pretrained_models
+import logging
+import open3d as o3d
+
+color_map_255 = {
+    0: np.array([100,204,42]),
+    1: np.array([50,104,250]),
+    2: np.array([105,244,244]),
+    3: np.array([249,239,60]),
+    4: np.array([227,109,223]),
+    5: np.array([100,106,249]),
+    6: np.array([199,200,98]),
+    7: np.array([159,113,191]),
+    8: np.array([224,62,31]),
+    9:np.array([201,95,99]),
+    10:np.array([95,202,103]),
+    11:np.array([200,200,200]),
+    12:np.array([50,50,50]),
+}
+
+color_map = {key : color_map_255[key]/255 for key in color_map_255}
+
+
+def main():
+    opt = OptInit().get_args()
+
+    logging.info('===> Creating dataloader...')
+    test_dataset = GeoData.S3DIS(opt.data_dir, opt.area, train=False, pre_transform=T.NormalizeScale())
+    test_loader = DenseDataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False, num_workers=0)
+    opt.n_classes = test_loader.dataset.num_classes
+    if opt.no_clutter:
+        opt.n_classes -= 1
+
+    logging.info('===> Loading the network ...')
+    model = DenseDeepGCN(opt).to(opt.device)
+    model, opt.best_value, opt.epoch = load_pretrained_models(model, opt.pretrained_model, opt.phase)
+
+    logging.info('===> Start Evaluation ...')
+    test(model, test_loader, opt)
+    
+
+
+def save_pcd(pos, labels, name, count):
+    '''
+    Issues due to crude implementation, as this is a test run:
+    The issue is that I don't take into account the batches, hence why I use "labels[0]", to work with the first
+    image from the batch. This is improper and it will also crash in the case that the batch size is 1.
+    This can be easily altered to take into account the batches (just segment the labels given the batch size), but
+    it's not necessary at this very moment.
+    '''
+    global color_map
+    labels = np.reshape(labels[0], (4096, 1))
+    colors = np.array([color_map[label[0]] for label in labels])
+    
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pos)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    o3d.io.write_point_cloud("./data/personal/" + name + "_" + str(count) + ".pcd", pcd)
+    
+
+
+def test(model, loader, opt):
+    Is = np.empty((len(loader), opt.n_classes))
+    Us = np.empty((len(loader), opt.n_classes))
+
+    model.eval()
+    data_iter = iter(loader) 
+
+    with torch.no_grad():
+        for i, data in enumerate(tqdm(loader)):
+            data = data.to(opt.device)
+            inputs = torch.cat((data.pos.transpose(2, 1).unsqueeze(3), data.x.transpose(2, 1).unsqueeze(3)), 1)
+            gt = data.y
+
+            out = model(inputs)
+            pred = out.max(dim=1)[1]
+
+            pred_np = pred.cpu().numpy()
+            target_np = gt.cpu().numpy()
+            
+            save_pcd(data.cpu().pos[0].numpy(), target_np, "target", i);
+            save_pcd(data.cpu().pos[0].numpy(), pred_np, "pred", i);
+
+            for cl in range(opt.n_classes):
+                cur_gt_mask = (target_np == cl)
+                cur_pred_mask = (pred_np == cl)
+                I = np.sum(np.logical_and(cur_pred_mask, cur_gt_mask), dtype=np.float32)
+                U = np.sum(np.logical_or(cur_pred_mask, cur_gt_mask), dtype=np.float32)
+                Is[i, cl] = I
+                Us[i, cl] = U
+
+    ious = np.divide(np.sum(Is, 0), np.sum(Us, 0))
+    ious[np.isnan(ious)] = 1
+    for cl in range(opt.n_classes):
+        logging.info("===> mIOU for class {}: {}".format(cl, ious[cl]))
+    logging.info("===> mIOU is {}".format(np.mean(ious)))
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ deep_gcns/deep_gcns_torch/examples/sem_seg_dense/test_vis.py
@@ -0,0 +1,202 @@
+import __init__
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch_geometric.datasets as GeoData
+from torch_geometric.data import DenseDataLoader
+import torch_geometric.transforms as T
+from config import OptInit
+from architecture import DenseDeepGCN
+from utils.ckpt_util import load_pretrained_models
+import logging
+import open3d as o3d
+import gcn_lib
+
+color_map_255 = {
+    0: np.array([100,204,42]),
+    1: np.array([50,104,250]),
+    2: np.array([105,244,244]),
+    3: np.array([249,239,60]),
+    4: np.array([227,109,223]),
+    5: np.array([100,106,249]),
+    6: np.array([199,200,98]),
+    7: np.array([159,113,191]),
+    8: np.array([224,62,31]),
+    9:np.array([201,95,99]),
+    10:np.array([95,202,103]),
+    11:np.array([200,200,200]),
+    12:np.array([50,50,50]),
+}
+
+color_map = {key : color_map_255[key]/255 for key in color_map_255}
+
+
+def main():
+    opt = OptInit().get_args()
+
+    logging.info('===> Creating dataloader...')
+    test_dataset = GeoData.S3DIS(opt.data_dir, opt.area, train=False, pre_transform=T.NormalizeScale())
+    test_loader = DenseDataLoader(test_dataset, batch_size=opt.batch_size, shuffle=False, num_workers=0)
+    opt.n_classes = test_loader.dataset.num_classes
+    if opt.no_clutter:
+        opt.n_classes -= 1
+
+    logging.info('===> Loading the network ...')
+    model = DenseDeepGCN(opt).to(opt.device)
+    print(model)
+    model, opt.best_value, opt.epoch = load_pretrained_models(model, opt.pretrained_model, opt.phase)
+
+    logging.info('===> Start Evaluation ...')
+    test(model, test_loader, opt)
+    
+
+
+def save_pcd(pos, labels, name, count):
+    '''
+    Issues due to crude implementation, as this is a test run:
+    The issue is that I don't take into account the batches, hence why I use "labels[0]", to work with the first
+    image from the batch. This is improper and it will also crash in the case that the batch size is 1.
+    This can be easily altered to take into account the batches (just segment the labels given the batch size), but
+    it's not necessary at this very moment.
+    '''
+    global color_map
+    labels = np.reshape(labels[0], (4096, 1))
+    colors = np.array([color_map[label[0]] for label in labels])
+    print(colors)
+    
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pos)
+    pcd.colors = o3d.utility.Vector3dVector(colors)
+    #print(pcd.has_colors())
+    o3d.io.write_point_cloud("./data/personal/" + name + "_" + str(count) + ".pcd", pcd)
+    
+
+
+def test(model, loader, opt):
+    Is = np.empty((len(loader), opt.n_classes))
+    Us = np.empty((len(loader), opt.n_classes))
+
+    model.eval()
+    
+    model_children = list(model.children())
+    layers = []
+            
+    for child in model_children:
+        
+        if type(child) == gcn_lib.dense.DenseDilatedKnnGraph:
+            layers.append(child)
+            #print("DilatedKNN: " + str(type(child)))
+        if type(child) == gcn_lib.dense.GraphConv2d:
+            layers.append(child)
+            #print("GraphConv2D: " + str(type(child)))
+        if type(child) == gcn_lib.dense.BasicConv:
+            layers.append(child)
+            #print("BasicConv: " + str(type(child)))      
+        if type(child) == torch.nn.Sequential:
+            #print("Sequential: " + str(type(child)))
+            for idx, nephew in enumerate(child.children()):
+                if type(nephew) == gcn_lib.dense.ResDynBlock2d:
+                    layers.append(nephew)
+                if type(nephew) == gcn_lib.dense.BasicConv:
+                    layers.append(nephew)
+                    #print("Basic conv: " + str(nephew))
+                if type(nephew) == torch.nn.Dropout:
+                    layers.append(nephew)
+       
+    with torch.no_grad():
+        data = next(iter(loader)) 
+        data = data.to(opt.device)
+        inputs = torch.cat((data.pos.transpose(2, 1).unsqueeze(3), data.x.transpose(2, 1).unsqueeze(3)), 1)
+        gt = data.y
+
+        # Recreating forward from DeepGCN
+        outputs = []
+        # Effectively creating the layers in a similar format to DeepGCN
+        # KNN dilation.
+        knn = layers.pop(0)
+        head = layers.pop(0)
+        backbone = layers[0:27]
+        fusion_block = layers[27]
+        final_layers = layers[28:]
+        # Utilizing the layers
+        dilated = knn(inputs[:, 0:3])
+        print("Dilated: ")
+        print(dilated.shape)
+        feats = [head(inputs, dilated)]
+        print("Head: ")
+        print(feats[0].shape)
+        print("Backbone: ")
+        for idx, layer in enumerate(backbone):
+            feats.append(layer(feats[-1]))   
+        print(str(feats[-1].shape) + ", but 27 times")
+
+        print("Concatenated post-backbone: ")
+        feats = torch.cat(feats, dim=1)
+        print(feats.shape)
+
+        
+        fusion = torch.max_pool2d(fusion_block(feats), kernel_size=[feats.shape[2], feats.shape[3]])
+        print("Max_pool2d: ")
+        print(fusion.shape)
+        fusion = torch.repeat_interleave(fusion, repeats=feats.shape[2], dim=2)
+        print("Repeat_interleave fusion: ")
+        print(fusion.shape)
+        result = final_layers[0](torch.cat((fusion, feats), dim=1))
+        print("BasicConv 1: ")
+        print(result.shape)
+        result = final_layers[1](result)
+        print("BasicConv 2: ")
+        print(result.shape)
+        result = final_layers[2](result)
+        print("Dropout: ")
+        print(result.shape)
+        result = final_layers[3](result)
+        print("Result: ")
+        result = result.squeeze(-1)
+        print(result.shape)
+        print(result)
+        pred = result.max(dim=1)[1]
+        print(pred.shape)
+
+        
+
+    '''
+    with torch.no_grad():
+        for i, data in enumerate(tqdm(loader)):
+            data = data.to(opt.device)
+            inputs = torch.cat((data.pos.transpose(2, 1).unsqueeze(3), data.x.transpose(2, 1).unsqueeze(3)), 1)
+            gt = data.y
+
+            out = model(inputs)
+            pred = out.max(dim=1)[1]
+
+            pred_np = pred.cpu().numpy()
+            target_np = gt.cpu().numpy()
+            
+            #print(data.cpu().pos[0].numpy())
+            #print(target_np.shape)
+            save_pcd(data.cpu().pos[0].numpy(), target_np, "target", i);
+            save_pcd(data.cpu().pos[0].numpy(), pred_np, "pred", i);
+
+
+            for cl in range(opt.n_classes):
+                cur_gt_mask = (target_np == cl)
+                cur_pred_mask = (pred_np == cl)
+                I = np.sum(np.logical_and(cur_pred_mask, cur_gt_mask), dtype=np.float32)
+                U = np.sum(np.logical_or(cur_pred_mask, cur_gt_mask), dtype=np.float32)
+                Is[i, cl] = I
+                Us[i, cl] = U
+    
+
+    ious = np.divide(np.sum(Is, 0), np.sum(Us, 0))
+    ious[np.isnan(ious)] = 1
+    for cl in range(opt.n_classes):
+        logging.info("===> mIOU for class {}: {}".format(cl, ious[cl]))
+    logging.info("===> mIOU is {}".format(np.mean(ious)))
+    '''
+
+
+if __name__ == '__main__':
+    main()
+
+
